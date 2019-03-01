20fe7f66b16e18e271cd8611ec021a32e4fbada6
taker-y <tak.10.3.blue@gmail.com>
2019-03-01 19:16:56 +0900

    #1 [update] lib/issues/cli.ex
    Implemented the function to return the value according to the argument

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,2 +1,36 @@
 defmodule Issues.CLI do
+  @default_display_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project.
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  'argv' can be -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally) the number of entries to format.
+
+  Return a tuple of '{ user_name, project_name, display_count }', or ':help' if help was given.
+  """
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean ],
+                                     aliases:  [ h:    :help    ])
+    case parse do
+    { [ help: true ], _, _ }
+      -> :help
+
+    { _, [ user_name, project_name, display_count ], _ }
+      -> { user_name, project_name, display_count }
+
+    { _, [ user_name, project_name ], _ }
+      -> { user_name, project_name, @default_display_count }
+
+    _ -> :help
+    end
+  end
 end
